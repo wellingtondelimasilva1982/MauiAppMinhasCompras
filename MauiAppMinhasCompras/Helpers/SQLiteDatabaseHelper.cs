3ee16289b9a48ea36f2e9ac3deab76dba2da4d2c
wellingtondelimasilva1982 <wellingtondelimasilva1982@gmail.com>
2025-03-17 09:42:38 -0300

    Agenda 4

--- MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MauiAppMinhasCompras.Models;
+using SQLite;
+
 
 namespace MauiAppMinhasCompras.Helpers
 {
-    internal class SQLiteDatabaseHelper
+    public class SQLiteDatabaseHelper
     {
+        readonly SQLiteAsyncConnection _connection;
+        
+        public SQLiteDatabaseHelper(string path)
+        { _connection = new SQLiteAsyncConnection(path);
+          _connection.CreateTableAsync<Produto>().Wait();
+        }
+
+        public Task<int> Insert(Produto p)
+        {
+            return _connection.InsertAsync(p);
+        }
+
+        public Task<List<Produto>> Update(Produto p)
+        {
+            string sql = "UPDATE Produto SET Descricao=?, Quantidade-?, Preco=? WHERE Id=?";
+
+            return _connection.QueryAsync<Produto>(sql, p.Descricao, p.Quantidade, p.Preco, p.Id);
+        }
+
+        public Task<int> Delete(int id) 
+        {
+            return _connection.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+
+        public Task<List<Produto>> GetAll() 
+        {
+            return _connection.Table<Produto>().ToListAsync();   
+        }
+
+        public Task<List<Produto>> Search(string q) 
+        {
+            string sql = "SELECT * FROM Produto WHERE descricao LIKE '%" + q + "%'";
+
+            return _connection.QueryAsync<Produto>(sql);
+        }
+
+
     }
 }
--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,9 +1,63 @@
+using MauiAppMinhasCompras.Models;
+using System.Collections.ObjectModel;
+
 namespace MauiAppMinhasCompras.Views;
 
-public partial class ListaProdutos : ContentPage
+public partial class ListaProduto : ContentPage
 {
-	public ListaProdutos()
+	ObservableCollection<Produto> lista = new ObservableCollection<Produto>(); 
+	public ListaProduto()
 	{
 		InitializeComponent();
+		
+		lst_produtos.ItemsSource = lista;
 	}
+
+    protected async override void OnAppearing()
+    {
+        List<Produto> tmp = await App.Db.GetAll();
+
+		tmp.ForEach(i => lista.Add(i));
+    }
+
+    private void ToolbarItem_Clicked(object sender, EventArgs e)
+	{
+		try
+		{
+			Navigation.PushAsync(new Views.NovoProduto());
+
+		} catch (Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "OK");
+		}
+	}
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+		string q = e.NewTextValue;
+
+		lista.Clear();
+
+        List<Produto> tmp = await App.Db.Search(q);
+
+        tmp.ForEach(i => lista.Add(i));
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+		double soma = lista.Sum(i => i.Total);
+
+		string msg = $"O total é {soma:C}";
+
+		DisplayAlert("Total dos Produtos", msg, "OK");
+    }
+
+    private void MenuItem_Clicked(object sender, EventArgs e)
+    {
+        var menuItem = sender as MenuItem;
+        if (menuItem?.BindingContext is Produto produtoSelecionado)
+        {
+            lista.Remove(produtoSelecionado);
+        }
+    }
 }
